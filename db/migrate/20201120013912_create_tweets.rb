71d78416767b8892f5a98846460f219a2412e3ca
daikiyamada32454 <waltz32454@gmail.com>
2020-11-26 12:18:15 +0900

    rubocop 修正

--- db/migrate/20201120013912_create_tweets.rb
+++ db/migrate/20201120013912_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string :tweet,        null:false
+      t.string :tweet, null: false
       t.text :image
       t.timestamps
     end
