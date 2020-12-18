8d512988ece92814519d2ee67b4219e97b3911ec
daikiyamada32454 <waltz32454@gmail.com>
2020-12-18 17:37:36 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "tweets#index"
-  resources :tweets, only:[:new,:create,:index]
+  resources :tweets, only:[:new,:create,:index, :show]
 end
