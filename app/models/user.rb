829c3e025f95d0d77f7787df79f89405e2b554e1
daikiyamada32454 <waltz32454@gmail.com>
2021-03-03 17:16:27 +0900

    パスワードのバリデーション 設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+  has_many :tweets
 
-         has_many :tweets
-         has_many :comments
-         
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
          with_options presence: true do
           validates :nickname
           validates :myinfo
+          validates :password, format: { with:/\A[a-zA-Z0-9]+\z/}
+         validates :password, length: { maximum: 12}
          end
 
 end
